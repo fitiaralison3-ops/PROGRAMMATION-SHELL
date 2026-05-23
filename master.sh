4d7550ff93738e4aea472d96e6b0422561e37b21
fitiaralison3-ops <fitiaralison3@gmail.com>
2026-05-23 21:54:08 +0300

    Refactor master.sh for improved readability and logging

--- master.sh
+++ master.sh
@@ -7,6 +7,8 @@ liste=/home/$nom/user.txt
 cle="$HOME/.ssh/id_contrall.pub" #mampiasa ny clé publique mba andefasana ny clé any amin'ny client
 key="$HOME/.ssh/id_contrall"
 #rhf misy clé dia tsy mila mot de passe intsony ny ssh
+LOG="$HOME/contrall.log" 
+#Soratana anaty log ny zava'misy rhtr
 
 if [ -f "$liste" ]; then
 	rm "$liste"
@@ -30,44 +32,53 @@ test=$(hostname -I | awk '{print $1}')
 
 ip_master="$test"
 debut=$(echo "$ip_master" | cut -d'.' -f1-3)
-
+tmpfile=$(mktemp)
+#mktemp sert à créer un fichier ou répertoire tenporaire
 for ((i=1; i<=254; i++))
 do
 	if [[ "$debut.$i" == "$ip_master" ]]; then continue
        	fi # tsy atao test ny ip an'ny master
 
-	if ping -c 1 -W 0.2 "$debut.$i" > /dev/null 2>&1; then	
-
+	if ping -c 1 -W 0.2 "$debut.$i" > /dev/null 2>&1; 
+	then	
+		echo "$debut.$i">>"$tmpfile"
+	fi &
+done
+wait
 	# chercher le nom de correspondant à chaque adresse IP trouvé	
-		hostname=$(host "$debut.$i" 2>/dev/null | grep pointer | awk '{print $NF}' | cut -d'.' -f1)
+while IFS= read -r ip ; do
+	hostname=$(host "$ip" 2>/dev/null | grep pointer | awk '{print $NF}' | cut -d'.' -f1)
 		user=$(echo "$hostname" | cut -d'-' -f1)
 
 		# Si le DNS n'a rien renvoyé, on demande à l'utilisateur de taper le nom
 		if [ -z "$user" ]; then
-        		echo -e "\n[!] Machine trouvée à l'adresse $debut.$i (Nom inconnu par le DNS)"
+        		echo -e "\n[!] Machine trouvée à l'adresse $ip (Nom inconnu par le DNS)"
       			read -p "Entrez le nom de l'utilisateur SSH pour cette machine : " user
 		fi
 
-        
 	#awk '{print $NF}: afficher le dernier mot
 	
 	## Demander si on envoit la clé au user trouvé
-		read -p "Voulez-vous donner la clé à $debut.$i ($user)? [o/n]: " affirmation
+		read -p "Voulez-vous donner la clé à $ip ($user)? [o/n]: " affirmation
 
 		if [[ "$affirmation" == "o" || "$affirmation" == "O" || "$affirmation" == "oui" ]]
 		then
 			read -s -p "Entrez le mot de passe de $user:" mdp
-			echo " "
-			if sshpass -p "$mdp" ssh-copy-id -o StrictHostKeyChecking=no -i "${cle}" "$user@$debut.$i"  ; then  #mila mot de passe donc tsy azo ampiana &
-				echo "$user:$debut.$i" >> "$liste" # copiena any @ fichier user.txt izy avy eo satria ilaina ao @ surveillance 
+			echo ""
+			if sshpass -p "$mdp" ssh-copy-id -o StrictHostKeyChecking=no -i "${cle}" "$user@$ip"  ; then  #mila mot de passe donc tsy azo ampiana &
+				# -o StrictHostKeyChecking=no : évite la question "Are you sure?" de SSH
+				echo "$user:$ip" >> "$liste" # copiena any @ fichier user.txt izy avy eo satria ilaina ao @ surveillance 
 				echo "Clé envoyée à $user"
+				echo "[$(date)] Clé envoyée à $user@$ip" >> "$LOG"
 			else
-    				echo "[ERREUR] Mauvais mot de passe ou connexion échouée pour $user@$debut.$i"
+    				echo "[ERREUR] Mauvais mot de passe ou connexion échouée pour $user@$ip"
+    				echo "[$(date)] Connexion avec $user@$ip échouée" >> "$LOG" 
 			fi
 		else
-			echo "$debut.$i ($user) ignoré"
+			echo "$ip ($user) ignoré"
 		fi
-	fi
-done
+done < "$tmpfile"
+
+rm "$tmpfile"
 
 echo " Scan de réseau et envoi des clés terminé"
